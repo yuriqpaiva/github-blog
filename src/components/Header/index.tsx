abd951bc32206e3227d4207a2e0a4c7f93d11734
Yuri Paiva <yuriqpaiva@gmail.com>
2023-05-05 15:42:20 -0300

    Add header background blur

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { HeaderContainer } from './styles'
+import { CoverBlur, HeaderContainer } from './styles'
 import coverEffectLeftImg from '../../assets/cover-effect-left.svg'
 import coverEffectRightImg from '../../assets/cover-effect-right.svg'
 import logoImg from '../../assets/github-blog.svg'
@@ -6,6 +6,7 @@ import logoImg from '../../assets/github-blog.svg'
 export function Header() {
   return (
     <HeaderContainer>
+      <CoverBlur />
       <img src={coverEffectLeftImg} alt="" height={188} />
       <img src={logoImg} alt="" height={98} />
       <img src={coverEffectRightImg} alt="" height={236} />
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,20 +1,22 @@
 import styled from 'styled-components'
+import coverBlur from '../../assets/cover-blur.svg'
 
 export const HeaderContainer = styled.header`
+  position: relative;
   background-color: ${({ theme }) => theme['base-profile']};
   width: 100%;
   height: 296px;
-
+  z-index: -1;
   display: flex;
   align-items: center;
   justify-content: center;
 
-  img:first-child {
+  img:nth-child(2) {
     position: absolute;
     left: 0;
   }
 
-  img:nth-child(2) {
+  img:nth-child(3) {
     margin-top: -70px;
   }
 
@@ -22,4 +24,12 @@ export const HeaderContainer = styled.header`
     position: absolute;
     right: 0;
   }
+`
+
+export const CoverBlur = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(${coverBlur}) no-repeat center;
+  background-size: cover;
 `
