6887e494dd7fe4979afe94dbb9027fcfdc196f25
Yuri Paiva <yuriqpaiva@gmail.com>
2023-05-06 19:14:06 -0300

    Add syntax highlighter to post markdown content

--- src/components/PostContent/index.tsx
+++ src/components/PostContent/index.tsx
@@ -1,5 +1,7 @@
 import { PostContentContainer } from './styles'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface Props {
   body: string
@@ -8,7 +10,30 @@ interface Props {
 export function PostContent({ body }: Props) {
   return (
     <PostContentContainer>
-      <ReactMarkdown linkTarget="_blank">{body}</ReactMarkdown>
+      <ReactMarkdown
+        linkTarget="_blank"
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '')
+            return !inline && match ? (
+              <SyntaxHighlighter
+                {...props}
+                style={oneDark}
+                language={match[1]}
+                PreTag="div"
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            )
+          },
+        }}
+      >
+        {body}
+      </ReactMarkdown>
     </PostContentContainer>
   )
 }
